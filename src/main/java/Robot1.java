66c4600178fc187a335770c02dc1282fdbc054d7
Ander-RL <anderrodriguez205@gmail.com>
2020-12-05 09:46:46 +0100

    Thread.sleep() añadidos

--- src/main/java/Robot1.java
+++ src/main/java/Robot1.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.awt.*;
 import java.util.concurrent.TimeUnit;
 
 public class Robot1 implements Runnable {
@@ -11,7 +12,7 @@ public class Robot1 implements Runnable {
     private static final String EMAIL = "Selenium3080@gmail.com";
     private static final String PASSWORD = "rtx30802020";
     private static final String NOMBRE = "Ander Rodriguez";
-    private static final String CARD = "**************";
+    private static final String CARD = "*******************";
     private static final String CV = "***";
 
     private Tienda tienda;
@@ -25,7 +26,8 @@ public class Robot1 implements Runnable {
         WebDriver driver = new ChromeDriver();
 
         // Enlace web
-        driver.get("https://www.pccomponentes.com/zotac-gaming-geforce-rtx-3080-trinity-10gb-gddr6x");
+        //driver.get("https://www.pccomponentes.com/zotac-gaming-geforce-rtx-3080-trinity-10gb-gddr6x");
+        driver.get("https://www.pccomponentes.com/gigabyte-geforce-rtx-3080-eagle-10gb-gddr6x");
         //driver.get("https://www.pccomponentes.com/evga-geforce-rtx-3080-xc3-black-gaming-10gb-gddr6x");
 
         // Espera para aceptar mensaje de Cookies
@@ -36,9 +38,21 @@ public class Robot1 implements Runnable {
         String estado = driver.findElement(By.xpath("//*[@id=\"btnsWishAddBuy\"]/button[3]")).getText().toUpperCase();
         System.out.println(estado);
 
+        // Recogemos el precio del producto
+        String precioCaracter = driver.findElement(By.xpath("//*[@id=\"precio-main\"]")).getText();
+        precioCaracter = precioCaracter.replace("€","");
+        precioCaracter = precioCaracter.replace(",",".");
+        double precio = Double.parseDouble(precioCaracter);
+
         // Comprobamos que el estado sea = COMPRAR si no lo es, refresca en bucle hasta que lo sea
-        while (!estado.equals("COMPRAR") && tienda.getParar() == false) {
+        while ((!estado.equals("COMPRAR") && tienda.getParar() == false) || precio > 800) {
             driver.navigate().refresh();
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                System.out.println("Sleep en Bucle");
+                e.printStackTrace();
+            }
             estado = driver.findElement(By.xpath("//*[@id=\"btnsWishAddBuy\"]/button[3]")).getText().toUpperCase();
             System.out.println(estado);
         }
@@ -82,6 +96,15 @@ public class Robot1 implements Runnable {
 
             // Aceptar Condiciones de envio y facturacion
             driver.findElement(By.xpath("//*[@id=\"ticket-pago\"]/p/label/span")).click();
+            try {
+                Thread.sleep(5000);
+                driver.findElement(By.xpath("//*[@id=\"ticket-pago\"]/p/label/span")).click();
+            } catch (InterruptedException e) {
+                System.out.println("Sleep en zona de compra");
+                e.printStackTrace();
+            }
+            /*WebDriverWait wait = new WebDriverWait(driver, 20);
+            wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"GTM-carrito-finalizarCompra\"]")));*/
             // Realizar compra
             driver.findElement(By.xpath("//*[@id=\"GTM-carrito-finalizarCompra\"]")).click();
 
--- src/main/java/Robot2.java
+++ src/main/java/Robot2.java
@@ -3,7 +3,10 @@ import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.awt.*;
 import java.util.concurrent.TimeUnit;
 
 public class Robot2 implements Runnable {
@@ -11,7 +14,7 @@ public class Robot2 implements Runnable {
     private static final String EMAIL = "Selenium3080@gmail.com";
     private static final String PASSWORD = "rtx30802020";
     private static final String NOMBRE = "Ander Rodriguez";
-    private static final String CARD = "**************";
+    private static final String CARD = "********************";
     private static final String CV = "***";
 
     private Tienda tienda;
@@ -25,8 +28,10 @@ public class Robot2 implements Runnable {
         WebDriver driver = new ChromeDriver();
 
         // Enlace web
-        //driver.get("https://www.pccomponentes.com/zotac-gaming-geforce-rtx-3080-trinity-10gb-gddr6x");
-        driver.get("https://www.pccomponentes.com/evga-geforce-rtx-3080-xc3-black-gaming-10gb-gddr6x");
+        driver.get("https://www.pccomponentes.com/zotac-gaming-geforce-rtx-3080-trinity-10gb-gddr6x");
+        //driver.get("https://www.pccomponentes.com/gigabyte-geforce-rtx-3080-eagle-10gb-gddr6x");
+        //driver.get("https://www.pccomponentes.com/evga-geforce-rtx-3080-xc3-black-gaming-10gb-gddr6x");
+        //driver.get("https://www.pccomponentes.com/ewent-spray-de-aire-comprimido-400ml");
 
         // Espera para aceptar mensaje de Cookies
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
@@ -36,9 +41,21 @@ public class Robot2 implements Runnable {
         String estado = driver.findElement(By.xpath("//*[@id=\"btnsWishAddBuy\"]/button[3]")).getText().toUpperCase();
         System.out.println(estado);
 
+        // Recogemos el precio del producto
+        String precioCaracter = driver.findElement(By.xpath("//*[@id=\"precio-main\"]")).getText();
+        precioCaracter = precioCaracter.replace("€","");
+        precioCaracter = precioCaracter.replace(",",".");
+        double precio = Double.parseDouble(precioCaracter);
+
         // Comprobamos que el estado sea = COMPRAR si no lo es, refresca en bucle hasta que lo sea
-        while (!estado.equals("COMPRAR") && tienda.getParar() == false) {
+        while ((!estado.equals("COMPRAR") && tienda.getParar() == false) || precio > 800) {
             driver.navigate().refresh();
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                System.out.println("Sleep en Bucle");
+                e.printStackTrace();
+            }
             estado = driver.findElement(By.xpath("//*[@id=\"btnsWishAddBuy\"]/button[3]")).getText().toUpperCase();
             System.out.println(estado);
         }
@@ -82,6 +99,14 @@ public class Robot2 implements Runnable {
 
             // Aceptar Condiciones de envio y facturacion
             driver.findElement(By.xpath("//*[@id=\"ticket-pago\"]/p/label/span")).click();
+            try {
+                Thread.sleep(5000);
+                driver.findElement(By.xpath("//*[@id=\"ticket-pago\"]/p/label/span")).click();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            /*WebDriverWait wait = new WebDriverWait(driver, 20);
+            wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"GTM-carrito-finalizarCompra\"]")));*/
             // Realizar compra
             driver.findElement(By.xpath("//*[@id=\"GTM-carrito-finalizarCompra\"]")).click();
 
